6503fd957092dafb05539b8e30cc908d75eecc7e
LAVISH SHARMA <56466819+lavish190@users.noreply.github.com>
2026-05-11 18:51:45 +0530

    ..

--- server/router/training.js
+++ server/router/training.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const authenticate = require("../middleware/authenticate"); // Uncomment only if auth is required
 const User = require("../models/userSchema");
 const { plantformAuditTemplate } = require("../utils/platformAuditTemplate");
-const transporter = require('./utils/transporter'); 
+const transporter = require('../utils/transporter'); 
 
 router.use(cookieParser());
 dotenv.config({ path: "./config.env" });
